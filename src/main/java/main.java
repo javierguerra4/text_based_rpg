31ddcb908655b0b8e6c02240543dd9403831c451
Javier Guerra <javier.j.guerra4@gmail.com>
2022-02-24 14:36:36 -0600

    Created the prompt

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,14 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("""
+                You wake up feeling well rested. What do you do first?
+                 A: Look out the front window.
+                 B: Drink water out of your bowl.
+                 C: Go to your humans door and whine until they wake up.""");
     }
 }
