bdf224370fe5bb070625fbf9c3cc93c0f163583f
Javier Guerra <javier.j.guerra4@gmail.com>
2022-02-24 15:11:18 -0600

    All scanner objects are working

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,5 +10,23 @@ public class main {
                  A: Look out the front window.
                  B: Drink water out of your bowl.
                  C: Go to your humans door and whine until they wake up.""");
+        String input = scan.nextLine();
+        if (input.equalsIgnoreCase("A")){
+            System.out.println("""
+            You look out the window and see that pesky child on his two round legs throwing paper at houses""");
+            Scanner sc = new Scanner(System.in);
+        } else if (input.equalsIgnoreCase("B")){
+            System.out.println("""
+                    You quench your thirst with delicious water.""");
+        } else if (input.equalsIgnoreCase("C")){
+            System.out.println("""
+                    Your human is awake. He does not look happy""");
+        } else {
+            System.out.println("""
+                    Please choose A, B, or C.
+                    A: Look out the front window.
+                     B: Drink water out of your bowl.
+                    C: Go to your humans door and whine until they wake up.""");
+        }
     }
 }
